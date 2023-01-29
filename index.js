4462348b9abe8fe8196ca548951f49be1f2d135d
md mahadi hasan <mahadihasan625601hstu@gmail.com>
2023-01-29 09:11:30 +0600

    server created manually by http request

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("mahadi");
+const http=require('http');
+
+const server=http.createServer((req,res)=>{
+    if(req.url==='/'){
+    res.write('Welcome to Node World')
+    res.end();
+    }
+    else if(req.url=='/mahadi'){
+        res.write('Mahadi you have to a industrious Person in real life');
+        res.end();
+    }
+    else{
+        res.write('Page Not Found');
+        res.end();
+    }
+});
+
+
+server.listen(4000,()=>{
+    console.log("Server is Running on port 4000");
+
+})
