878856f55af1c4ce25a589136cff6258079597e7
md mahadi hasan <mahadihasan625601hstu@gmail.com>
2023-01-30 23:11:05 +0600

    response send json response send and send send status

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 const express=require('express');
-
-
-
-
 const app=express();
 
 
-
-  
-
-app.set('view engin','ejs');
+// app.set('view engin','ejs');
 
 
 app.get('/about',(req,res)=>{
-    res.render('pages/about.ejs',{
-        name:"United Bangladesh"
-    });
-    console.log(req.route);
+//    res.send('This is about page');
+// res.json({
+//     name:"Md Mahadi Hasan",
+//     uiversity:"HSTU"
+// })
+  res.sendStatus(403);
 })
 
 
